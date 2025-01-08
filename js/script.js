dce3a9889883ed45bfc37e4903eb194f12ea5ab8
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-09 00:08:03 +0100

    [feat] Add array

--- js/script.js
+++ js/script.js
@@ -7,17 +7,29 @@ let eMail = (prompt ("Inserisci la tua e-mail)"));
 
 
 // variabile di messaggio/risultato
-let result;
+let result = false;
 
 
 // verifica della presenza dell'e-mail nella lista degli autorizzati all'accesso
 
+for (let i = 0; i < listGuests.length; i++) {
+
     // SE l'e-mail è inclusa nella lista
+    if (email === listGuests [i]) {
+        result = true;
+        break;
+    }
+
+}
     
         // setto il risultato sull'accesso autorizzato all'utente
-
+        if(result){
+            console.log("Sei nella lista, puoi entrare")
+        } else {
     // SE l'e-mail non è inclusa nella lista 
         // setto il risultato sull'accesso negato all'utente
+        console.log("Non sei nella lista, non puoi entrare")
+        }
 
 
 // indicare all'utente se può accedere o meno --- OUTPUT
