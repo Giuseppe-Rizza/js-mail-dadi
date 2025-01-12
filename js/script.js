4d2d0879e76c6ce350c2d68b2cea38a863157431
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-12 12:30:58 +0100

    [feat] Correct mail exercise

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
 // creazione array con elenco della lista degli ospiti
 const listGuests = ["pinkopallo@hotmail.it", "pinkopallo@gmail.com", "pinkopallo@tiscali.it", "pinkopallo@libero.it", "pinkopallo@alice.it", "pinkopallo@virgilio.it", "pinkopallo@yahoo.it", ]
 
+console.log(listGuests);
+
+
 
 // richiesta info all'utente (e-mail)
 let eMail = (prompt ("Inserisci la tua e-mail)"));
 
+console.log(eMail);
+
+
 
 // variabile di messaggio/risultato
-let result = false;
+let searchEmail = false;
 
 
 // verifica della presenza dell'e-mail nella lista degli autorizzati all'accesso
 
 for (let i = 0; i < listGuests.length; i++) {
 
-    // SE l'e-mail è inclusa nella lista
-    if (email === listGuests [i]) {
-        result = true;
-        break;
-    }
+    let ifEmailPresent = listGuests [i];
+
+    if (eMail === ifEmailPresent)
+        console.log("Puoi accedere");
+        
+    if (eMail !== ifEmailPresent)
+        console.log("Non puoi accedere");
 
 }
-    
-        // setto il risultato sull'accesso autorizzato all'utente
-        if(result){
-            console.log("Sei nella lista, puoi entrare")
-        } else {
-    // SE l'e-mail non è inclusa nella lista 
-        // setto il risultato sull'accesso negato all'utente
-        console.log("Non sei nella lista, non puoi entrare")
-        }
-
-
-// indicare all'utente se può accedere o meno --- OUTPUT
