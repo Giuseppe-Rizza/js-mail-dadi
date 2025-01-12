2826471b5cb82381b0b880680c066ae0d2b5ac23
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-12 16:39:57 +0100

    [feat] Add part two js

---
+++ js/Dadi script.js
@@ -0,0 +1,34 @@
+// creazione array con elenco delle facce del dado
+
+const numbersDice = [1, 2, 3, 4, 5, 6];
+
+console.log(numbersDice);
+
+
+let userNumber = Math.floor(Math.random()*7)
+
+let pcNumber = Math.floor(Math.random()*7)
+
+console.log(userNumber);
+
+console.log(pcNumber);
+
+if (userNumber > pcNumber) {
+
+    console.log("User win");
+
+}
+
+else if (userNumber < pcNumber) {
+
+    console.log("PC win");
+    
+}
+
+else {
+
+    console.log("Draw");
+    
+}
+
+
